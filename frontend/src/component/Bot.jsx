0664cfa760aef34cb8681519514c390ae1043601
rachit-taneja <tanejarachit111@gmail.com>
2026-06-08 20:42:53 +0530

    New changes

--- frontend/src/component/Bot.jsx
+++ frontend/src/component/Bot.jsx
@@ -16,7 +16,7 @@ function Bot() {
 
     try {
   const res = await axios.post(
-  `${import.meta.env.VITE_API_URL}/bot/v1/message`,
+  `${process.env.REACT_APP_API_URL}/bot/v1/message`,
   {
     text: input,
   }
